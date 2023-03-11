cada2ee0d099d3c2820e9bf45e919ae4006127d6
Kevin Milacki <kevin.milacki@gmail.com>
2023-03-11 16:52:55 -0500

    added game info page and all images

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const navT = document.querySelector(".navbar_toggle");
 const links = document.querySelector(".main_nav");
 const hamburger = document.querySelector(".hamburger");
 const headContainer = document.querySelector(".head_container");
+const accord = document.querySelectorAll(".accord_head");
+
 
 // console.log(hamburger);
 
@@ -11,3 +13,17 @@ navT.addEventListener('click', () => {
         hamburger.classList.toggle('menu_open');
         headContainer.classList.toggle('navAct');
     })
+
+
+accord.forEach(accord => {
+    accord.addEventListener("click", event => {
+        accord.classList.toggle("active");
+        const accordBody = accord.nextElementSibling;
+        if (accord.classList.contains("active")) {
+            accordBody.style.maxHeight = accordBody.scrollHeight + "px";
+        }
+        else {
+            accordBody.style.maxHeight = 0;
+        }
+    })
+})

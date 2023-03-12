df3bd0732ef532ab2e8ab3a81531e243b42f813d
Kevin Milacki <kevin.milacki@gmail.com>
2023-03-12 13:21:19 -0400

    Added Highscores page and updated booking page

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const links = document.querySelector(".main_nav");
 const hamburger = document.querySelector(".hamburger");
 const headContainer = document.querySelector(".head_container");
 const accord = document.querySelectorAll(".accord_head");
-
+const roomType = document.getElementById("room_type");
+const bgImg = document.querySelector(".pr_container");
+const medQ = window.matchMedia("(max-width: 932px)");
 
 // console.log(hamburger);
 
@@ -26,4 +28,30 @@ accord.forEach(accord => {
             accordBody.style.maxHeight = 0;
         }
     })
+})
+
+roomType.addEventListener("change", () => {
+    
+    if (roomType.value === "birthday") {
+        if (medQ.matches){
+            bgImg.style.backgroundImage = "linear-gradient(to bottom, transparent 50%,rgb(16, 4, 76)) , url(images/party_room.jpg)";
+        } else {
+            bgImg.style.backgroundImage = "linear-gradient(to right, transparent 50%,rgb(16, 4, 76)) , url(images/party_room.jpg)";
+        }
+          
+    } else if (roomType.value === "pr_arcade")  {
+        if (medQ.matches){    
+            bgImg.style.backgroundImage = "linear-gradient(to bottom, transparent 50%,rgb(16, 4, 76)) , url(images/party_room_6.png)";
+        } else {
+            bgImg.style.backgroundImage = "linear-gradient(to right, transparent 50%,rgb(16, 4, 76)) , url(images/party_room_6.png)";
+        }
+    
+    } else {
+        if (medQ.matches){
+            bgImg.style.backgroundImage = "linear-gradient(to bottom, transparent 50%,rgb(16, 4, 76)) , url(images/party_room_3.jpg)";
+        } else {
+            bgImg.style.backgroundImage = "linear-gradient(to right, transparent 50%,rgb(16, 4, 76)) , url(images/party_room_3.jpg)";
+        }
+    }    
+    
 })
